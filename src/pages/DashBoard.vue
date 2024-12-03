283e6e129d2dc5789978951276b873045a795622
vi.dn <vi.DN@mor.com.vn>
2024-12-03 17:22:26 +0700

    Add dockerfile

--- src/pages/DashBoard.vue
+++ src/pages/DashBoard.vue
@@ -1,19 +1,20 @@
 <template>
-    <div>
-      <h1>Dashboard</h1>
-      <p>Chào mừng bạn đến với Dashboard!</p>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  export default {
-    name: 'DashBoard',
-  };
-  </script>
-  
-  <style scoped>
-  h1 {
-    color: blue;
-  }
-  </style>
-  
+  <div>
+    <h1 class='m-1'>Dashboard</h1>
+    <p class='text-black'>Chào mừng bạn đến với Dashboard!</p>
+    <n-button type="tertiary"> Tertiary </n-button>
+    <n-button type="info"> Info </n-button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'DashBoard',
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
