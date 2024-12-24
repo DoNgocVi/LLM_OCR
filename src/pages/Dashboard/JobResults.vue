dcc26237c73c61492c39a518cb011e59b2e94720
ngocvi <vi.dn@mor.com.vn>
2024-12-24 15:17:10 +0700

    :bug: fix(desktop): remove mock data in job result screen

--- src/pages/Dashboard/JobResults.vue
+++ src/pages/Dashboard/JobResults.vue
@@ -22,16 +22,6 @@
         <n-select v-model:value="value1" :options="defaultOptionSelect" />
       </div>
     </div>
-    <div class="mt-4">
-      <n-data-table
-        :columns="columns"
-        :data="data"
-        :pagination="pagination"
-        :bordered="false"
-        :on-update:page="handlePageChange"
-        :loading="isLoading"
-      />
-    </div>
   </div>
 
   <n-modal
@@ -55,7 +45,7 @@
 </template>
 
 <script setup lang="ts">
-  import { DataTableColumns, NButton, useMessage } from 'naive-ui'
+  import { useMessage } from 'naive-ui'
   import { useCommonStore } from '@/stores/commonStore'
   import { storeToRefs } from 'pinia'
   import { Search } from '@vicons/ionicons5'
@@ -63,78 +53,10 @@
   const commonStore = useCommonStore()
   const {} = storeToRefs(commonStore)
   const value1 = ref()
-  type Song = {
-    no: number
-    title: string
-    length: string
-  }
+
   const message = useMessage()
-  const pagination = ref({
-    pageSize: 10
-  })
-  const isLoading = ref(false)
-  const data = ref(createData())
   const showModal = ref(false)
   const showModalRef = ref(false)
-
-  function createData() {
-    return Array.from({ length: 100 }).map((_, index) => ({
-      no: index,
-      title: `John Brown ${index}`,
-      length: (Math.random() * 40) | 0,
-      address: `New York No. ${index} Lake Park`
-    }))
-  }
-
-  const createColumns = ({ play }: { play: (row: Song) => void }): DataTableColumns<Song> => {
-    return [
-      {
-        title: 'No',
-        key: 'no'
-      },
-      {
-        title: 'Title',
-        key: 'title'
-      },
-      {
-        title: 'Length',
-        key: 'length'
-      },
-      {
-        title: 'Action',
-        key: 'actions',
-        render(row) {
-          return h(
-            NButton,
-            {
-              strong: true,
-              tertiary: true,
-              size: 'small',
-              onClick: () => play(row)
-            },
-            { default: () => 'Play' }
-          )
-        }
-      }
-    ]
-  }
-  const columns = ref(
-    createColumns({
-      play(row: Song) {
-        message.info(`Play ${row.title}`)
-      }
-    })
-  )
-  const handlePageChange = (page: number) => {
-    // const infoPage = {
-    //   page
-    // }
-    console.log(page, 'page')
-    isLoading.value = true
-    setTimeout(() => {
-      isLoading.value = false
-    }, 200)
-  }
   const onPositiveClick = () => {
     message.success('Submit')
     showModalRef.value = false
